c2cca78b6b06771635a2a906ee9c679e09fc9619
doctor-rutvik14 <rutvik14d@gmail.com>
2020-08-06 21:51:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useState}from 'react';
 import './App.css';
 
 function App() {
+  const [num,setNum] = useState(0);  
+  const Incr = () => {
+    setNum(num + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{num}</h1>
+     <button onClick={Incr}>Click to Increase count</button>
     </div>
   );
 }
